2a7bf0c7a1d701de668ed4fbaf4041d7f7bd0d74
joblessmf <ndimitrov992@gmail.com>
2025-08-06 20:42:33 +0300

    Initial Commit

---
+++ Friend Face/Friend_FaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Friend_FaceApp.swift
+//  Friend Face
+//
+//  Created by Nikolay Simeonov on 6.08.25.
+//
+
+import SwiftUI
+
+@main
+struct Friend_FaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
